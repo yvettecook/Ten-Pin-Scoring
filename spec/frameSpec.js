5c73d978f4654456297ac55619dfafd079dede30
yvettecook <yvettecook@gmail.com>
2014-10-18 10:31:25 +0100

    Starting frames - can currently contain throws

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,5 +1,16 @@
 describe('Frames', function() {
 
-	it
+	it('contains one throw', function() {
+		frame1 = new Frame;
+		frame1.throw1 = new Throw;
+		expect(frame1.throw1).toEqual(jasmine.any(Throw));
+	});
+
+	it('can contain two throws', function() {
+		frame1 = new Frame;
+		frame1.throw1 = new Throw;
+		frame1.throw2 = new Throw;
+		expect(frame1.throw2).toEqual(jasmine.any(Throw));
+	});
 
 });
